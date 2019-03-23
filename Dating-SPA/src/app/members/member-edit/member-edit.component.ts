290cdf2db24d2d7a437c25dcf87363e5a66bc0a1
YairFernando67 <yair.facio11@gmail.com>
2019-03-23 17:39:27 -0600

    End of Module 11

--- Dating-SPA/src/app/members/member-edit/member-edit.component.ts
+++ Dating-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -14,6 +14,7 @@ import { UserService } from 'src/app/_services/user.service';
 export class MemberEditComponent implements OnInit {
   @ViewChild('editForm') editForm: NgForm;
   user: User;
+  photoUrl: string;
   @HostListener('window:beforeunload', ['$event'])
   unloadNotification($event: any) {
     if (this.editForm.dirty) {
@@ -29,6 +30,7 @@ export class MemberEditComponent implements OnInit {
 // tslint:disable-next-line: no-string-literal
       this.user = data['user'];
     });
+    this.authService.currentPhotoUrl.subscribe(photoUrl => this.photoUrl = photoUrl);
   }
 
   updateUser() {
@@ -39,6 +41,10 @@ export class MemberEditComponent implements OnInit {
     }, error => {
       this.alertify.error(error);
     });
+  }
+
+  updateMainPhoto(photoUrl) {
+    this.user.photoUrl = photoUrl;
   }
 
 }
--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -15,7 +15,7 @@ namespace DatingApp.API.Data
         }
         public async Task<User> Login(string username, string password)
         {
-            var user = await _context.Users.FirstOrDefaultAsync(p => p.Username == username);
+            var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(p => p.Username == username);
 
             if (user == null)
                return null;
